20cdcb32ff0a11886fa94a34558ed94db6f04248
Jake Simpson <67084246+jakelsimpson@users.noreply.github.com>
2024-01-15 00:54:47 -0500

    Playing with APIS
    Playing with the Pokemon API for testing

--- renderer.js
+++ renderer.js
@@ -7,3 +7,19 @@ const func = async () => {
 }
 
 func();
+
+// let testthis = window.location.pathname.split("/");
+// let currentpage = testthis[testthis.length - 1];
+
+
+// if (currentpage === "index.html") {
+//     window.location.replace("pages/sign-in.html");
+// }
+
+const get_pokemon = async () => {
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon/ditto");
+    const gotit = await response.json();
+    console.log(gotit);
+}
+
+get_pokemon();
